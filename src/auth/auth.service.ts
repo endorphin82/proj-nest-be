e630fa366b6e9abcc2793b1d806e1dfbe1a7e5cc
endorphin82 <endorphin82@gmail.com>
2020-07-29 16:43:26 +0300

    forgot

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -18,16 +18,16 @@ import * as moment from 'moment'
 import { statusEnum } from '../user/enums/status.enum'
 import { IReadableUser } from '../user/interfaces/readable-user.interface'
 import { SignInDto } from './dto/signin.dto'
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcrypt'
 import { ChangePasswordDto } from './dto/change-password.dto'
 import { ITokenPayload } from './interfaces/token-payload.interface'
 import { userSensitiveFieldsEnum } from '../user/enums/protected-fields.enum'
-import * as _ from 'lodash';
+import * as _ from 'lodash'
 import { ForgotPasswordDto } from './dto/forgot-password.dto'
 
 @Injectable()
 export class AuthService {
-  private readonly clientAppUrl: string;
+  private readonly clientAppUrl: string
 
   constructor(
     private readonly jwtService: JwtService,
@@ -36,75 +36,76 @@ export class AuthService {
     private readonly configService: ConfigService,
     private readonly mailService: MailService,
   ) {
-    this.clientAppUrl = this.configService.get<string>('FE_APP_URL');
+    this.clientAppUrl = this.configService.get<string>('FE_APP_URL')
   }
 
   async signUp(createUserDto: CreateUserDto): Promise<boolean> {
-    const user = await this.userService.create(createUserDto, [roleEnum.user]);
-    await this.sendConfirmation(user);
-    return true;
+    const user = await this.userService.create(createUserDto, [roleEnum.user])
+    await this.sendConfirmation(user)
+    return true
   }
 
   async signIn({ email, password }: SignInDto): Promise<IReadableUser> {
-    const user = await this.userService.findByEmail(email);
+    const user = await this.userService.findByEmail(email)
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      const token = await this.signUser(user);
-      const readableUser = user.toObject() as IReadableUser;
-      readableUser.accessToken = token;
+      const token = await this.signUser(user)
+      const readableUser = user.toObject() as IReadableUser
+      readableUser.accessToken = token
 
-      return _.omit<any>(readableUser, Object.values(userSensitiveFieldsEnum)) as IReadableUser;
+      return _.omit<any>(readableUser, Object.values(userSensitiveFieldsEnum)) as IReadableUser
     }
-    throw new BadRequestException('Invalid credentials');
+    throw new BadRequestException('Invalid credentials')
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-inferrable-types
   async signUser(user: IUser, withStatusCheck: boolean = true): Promise<string> {
     if (withStatusCheck && (user.status !== statusEnum.active)) {
-      throw new MethodNotAllowedException();
+      throw new MethodNotAllowedException()
     }
     const tokenPayload: ITokenPayload = {
       _id: user._id,
       status: user.status,
       roles: user.roles,
-    };
-    const token = await this.generateToken(tokenPayload);
+    }
+    const token = await this.generateToken(tokenPayload)
     const expireAt = moment()
       .add(1, 'day')
-      .toISOString();
+      .toISOString()
 
     await this.saveToken({
       token,
       expireAt,
       uId: user._id,
-    });
+    })
 
-    return token;
+    return token
   }
 
   async changePassword(userId: string, changePasswordDto: ChangePasswordDto): Promise<boolean> {
-    const password = await this.userService.hashPassword(changePasswordDto.password);
+    const password = await this.userService.hashPassword(changePasswordDto.password)
 
-    await this.userService.update(userId, { password });
-    await this.tokenService.deleteAll(userId);
-    return true;
+    await this.userService.update(userId, { password })
+    await this.tokenService.deleteAll(userId)
+    return true
   }
 
   async confirm(token: string): Promise<IUser> {
-    const data = await this.verifyToken(token);
-    const user = await this.userService.find(data._id);
+    const data = await this.verifyToken(token)
+    const user = await this.userService.find(data._id)
 
-    await this.tokenService.delete(data._id, token);
+    await this.tokenService.delete(data._id, token)
 
     if (user && user.status === statusEnum.pending) {
-      user.status = statusEnum.active;
-      return user.save();
+      user.status = statusEnum.active
+      return user.save()
     }
-    throw new BadRequestException('Confirmation error');
+    throw new BadRequestException('Confirmation error')
   }
 
   async sendConfirmation(user: IUser) {
-    const token = await this.signUser(user, false);
-    const confirmLink = `${this.clientAppUrl}/auth/confirm?token=${token}`;
+    const token = await this.signUser(user, false)
+    const confirmLink = `${this.clientAppUrl}/auth/confirm?token=${token}`
 
     await this.mailService.send({
       from: this.configService.get<string>('JS_CODE_MAIL'),
@@ -114,40 +115,40 @@ export class AuthService {
                 <h3>Hello ${user.firstName}!</h3>
                 <p>Please use this <a href="${confirmLink}">link</a> to confirm your account.</p>
             `,
-    });
+    })
   }
 
   private async generateToken(data: ITokenPayload, options?: SignOptions): Promise<string> {
-    return this.jwtService.sign(data, options);
+    return this.jwtService.sign(data, options)
   }
 
   private async verifyToken(token): Promise<any> {
     try {
-      const data = this.jwtService.verify(token) as ITokenPayload;
-      const tokenExists = await this.tokenService.exists(data._id, token);
+      const data = this.jwtService.verify(token) as ITokenPayload
+      const tokenExists = await this.tokenService.exists(data._id, token)
 
       if (tokenExists) {
-        return data;
+        return data
       }
-      throw new UnauthorizedException();
+      throw new UnauthorizedException()
     } catch (error) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException()
     }
   }
 
   private async saveToken(createUserTokenDto: CreateUserTokenDto) {
-    const userToken = await this.tokenService.create(createUserTokenDto);
+    const userToken = await this.tokenService.create(createUserTokenDto)
 
-    return userToken;
+    return userToken
   }
 
   async forgotPassword(forgotPasswordDto: ForgotPasswordDto): Promise<void> {
-    const user = await this.userService.findByEmail(forgotPasswordDto.email);
+    const user = await this.userService.findByEmail(forgotPasswordDto.email)
     if (!user) {
-      throw new BadRequestException('Invalid email');
+      throw new BadRequestException('Invalid email')
     }
-    const token = await this.signUser(user);
-    const forgotLink = `${this.clientAppUrl}/auth/forgotPassword?token=${token}`;
+    const token = await this.signUser(user)
+    const forgotLink = `${this.clientAppUrl}/auth/forgotPassword?token=${token}`
 
     await this.mailService.send({
       from: this.configService.get<string>('JS_CODE_MAIL'),
@@ -157,6 +158,6 @@ export class AuthService {
                 <h3>Hello ${user.firstName}!</h3>
                 <p>Please use this <a href="${forgotLink}">link</a> to reset your password.</p>
             `,
-    });
+    })
   }
 }
