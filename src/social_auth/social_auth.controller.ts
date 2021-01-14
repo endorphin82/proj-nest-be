f7717381a6814839695d8f080c043de224bbf793
endorphin82 <endorphin82@gmail.com>
2021-01-14 13:49:20 +0200

    post

--- src/social_auth/social_auth.controller.ts
+++ src/social_auth/social_auth.controller.ts
@@ -1,5 +1,5 @@
 import { ApiTags } from '@nestjs/swagger'
-import { Controller, Get, Query, UseGuards, ValidationPipe } from '@nestjs/common'
+import { Controller, Get, Post, Query, UseGuards, ValidationPipe } from '@nestjs/common'
 import { SocialAuthService } from './social_auth.service'
 import { AuthGuard } from '@nestjs/passport'
 import { CreateSocialAuthDto } from './dto/create-social-auth.dto'
@@ -18,8 +18,15 @@ export class SocialAuthController {
     return req
   }
 
+  // @UseGuards(AuthGuard('google'))
+  // @Get('/auth/google/callback')
+  // async getTokenAfterGoogleSignIn(@Query(new ValidationPipe()) req: CreateSocialAuthDto) {
+  //   console.log('!!!getTokenAfterGoogleSignIn', req.uId)
+  //   return req.uId
+  // }
+
   @UseGuards(AuthGuard('google'))
-  @Get('/auth/google/callback')
+  @Post('/auth/google/callback')
   async getTokenAfterGoogleSignIn(@Query(new ValidationPipe()) req: CreateSocialAuthDto) {
     console.log('!!!getTokenAfterGoogleSignIn', req.uId)
     return req.uId
