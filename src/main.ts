4e17bd5097bc33f2d20e0dbb06f75e6823ecba84
endorphin82 <endorphin82@gmail.com>
2021-01-19 13:53:39 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  // app.enableCors()
+  app.enableCors()
 
   const options = new DocumentBuilder()
     .addBearerAuth()
